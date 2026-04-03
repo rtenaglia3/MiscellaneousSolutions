dcc1630ac4c3a87ace284b8cc96a2ec20855025f
Rocco <rtenaglia3@gmail.com>
2026-04-03 13:37:56 -0400

    merge and rebase

--- Java/ContiguousOnes.java
+++ Java/ContiguousOnes.java
@@ -1,9 +1,8 @@
+package Java;
 /// Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
 
 class Solution {
     public boolean checkOnesSegment(String s) {
-        boolean contiguousSegmentFound = false;
-
         int firstOne = s.indexOf('1');
 
         if (firstOne == -1) return true;
